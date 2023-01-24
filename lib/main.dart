60762791001853ce382bb5cd0fed00efed7c5a9f
akbar-human <akbar@hub9.biz>
2023-01-24 18:01:09 +0700

    create transaction model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_guide/models/transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +16,46 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final List<Transaction> listTransaction = [
+    Transaction(
+      id: "t1",
+      title: "Goppay",
+      amount: 200000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Goppay-2",
+      amount: 200000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Goppay-3",
+      amount: 200000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Goppay-4",
+      amount: 200000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Goppay-5",
+      amount: 200000,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +63,8 @@ class MyHomePage extends StatelessWidget {
         title: const Text("Expense Plan"),
       ),
       body: Column(
-        children: const [
-          SizedBox(
+        children: [
+          const SizedBox(
             width: double.infinity,
             child: Card(
               color: Colors.blue,
@@ -38,9 +72,17 @@ class MyHomePage extends StatelessWidget {
               child: Text("Chart"),
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Text("LIST of TX"),
+          Column(
+            children: listTransaction
+                .map((e) => Card(
+                      child: Row(
+                        children: [
+                          Container(child: Text(e.amount.toString())),
+                          Container(child: Text(e.title)),
+                        ],
+                      ),
+                    ))
+                .toList(),
           ),
         ],
       ),
